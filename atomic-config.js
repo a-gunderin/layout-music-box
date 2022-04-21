3ccec54c255f5951a160559c73c984af48676f1b
Andrey Gunderin <ga@web-notes.info>
2022-04-21 04:14:46 +0300

    Step 2

--- atomic-config.js
+++ atomic-config.js
@@ -1,8 +1,14 @@
 module.exports = {
 	'breakPoints': {
-		'xs': '@media screen and (max-width:700px)',
-		'sm': '@media screen and (min-width:700px)',
-		'md': '@media screen and (min-width:999px)',
-		'lg': '@media screen and (min-width:1200px)'
+		'xs': '@media screen and (max-width:450px)',
+		'sm': '@media screen and (max-width:700px)',
+		'md': '@media screen and (max-width:999px)',
+		'lg': '@media screen and (max-width:1240px)'
+	},
+	'custom': {
+		roboto: 'Roboto',
+		banner: 'url(../images/banner.png)',
+		textColor: '#cacaca',
+		bgColor: '#08020d',
 	},
 };
