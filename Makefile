3221b1613e7167ae8b0083c6e06ba6c00ec622a6
Andrey Gunderin <ga@web-notes.info>
2022-04-21 03:29:30 +0300

    Change sass on atomizer

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 install:
 	npm ci
-	npm run scss
+	npm run atomizer
 
 lint:
 	npx stylelint ./src/css/*.css
---
+++ atomic-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+	'breakPoints': {
+		'xs': '@media screen and (max-width:700px)',
+		'sm': '@media screen and (min-width:700px)',
+		'md': '@media screen and (min-width:999px)',
+		'lg': '@media screen and (min-width:1200px)'
+	},
+};
