c98f21685ec6a8211571d1982a32a35b7a8aae6e
Andrey Gunderin <ga@web-notes.info>
2022-04-22 23:24:45 +0300

    Step 3

--- atomic-config.js
+++ atomic-config.js
@@ -6,9 +6,17 @@ module.exports = {
 		'lg': '@media (max-width:1239px)'
 	},
 	'custom': {
+		// FONTS
 		roboto: 'Roboto, Verdana, sans-serif',
 		btnFont: 'Arial, Verdana, sans-serif',
+
+		// BACKGROUND IMAGES
 		banner: 'url("../images/banner.png")',
+		subscribe_1: 'url("../images/subscribe_1.png")',
+		subscribe_2: 'url("../images/subscribe_2.png")',
+		subscribe_3: 'url("../images/subscribe_3.png")',
+
+		// COLORS
 		textColor: '#cacaca',
 		bgColor: '#08020d',
 		darkGrey: '#3c3b3d',
