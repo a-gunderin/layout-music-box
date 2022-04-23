95f933c11f60634d987f1255c4dcd02e78434e4a
Andrey Gunderin <ga@web-notes.info>
2022-04-23 04:58:11 +0300

    Step 6

--- atomic-config.js
+++ atomic-config.js
@@ -10,16 +10,18 @@ module.exports = {
 		roboto: 'Roboto, Verdana, sans-serif',
 		btnFont: 'Arial, Verdana, sans-serif',
 
-		// BACKGROUND IMAGES
+		// BACKGROUND IMAGES & GRADIENTS
 		banner: 'url("../images/banner.png")',
 		subscribe_1: 'url("../images/subscribe_1.png")',
 		subscribe_2: 'url("../images/subscribe_2.png")',
 		subscribe_3: 'url("../images/subscribe_3.png")',
+		gradient: 'linear-gradient(89.75deg, #5328a1 32.83%, #95390c 81.56%)',
 
 		// COLORS
 		textColor: '#cacaca',
 		bgColor: '#08020d',
 		darkGrey: '#3c3b3d',
+		lightGrey: '#e0e0e0',
 		buttonColor1: '#34C176',
 		buttonColor2: '#2ba564',
 		orange1: '#f89202',
